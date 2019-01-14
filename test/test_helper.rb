2eab3cdc8612df214f216cffaef569e5b4c32b15
Jason Hsu <jhsu802701@jasonhsu.com>
2019-01-14 21:22:38 +0000

    Add user login capability

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -71,3 +71,16 @@ def sign_up_user(name_u, name_l, name_f, e, p1, p2)
   click_button('Sign up')
 end
 # rubocop:enable Metrics/ParameterLists
+
+def login_user(str_login, str_pwd, status_remember)
+  visit root_path
+  click_on 'Login'
+  fill_in('Login', with: str_login)
+  fill_in('Password', with: str_pwd)
+  if status_remember == true
+    check('Remember me')
+  else
+    uncheck('Remember me')
+  end
+  click_button('Log in')
+end
